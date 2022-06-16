80a5ae0c4d1aa7db0acc6cbb0d5deb54f9204097
hojin <redsmile123@naver.com>
2022-06-16 16:37:08 +0900

    update :

--- src/main/java/Pack/vo/LogiExportVo.java
+++ src/main/java/Pack/vo/LogiExportVo.java
@@ -8,20 +8,23 @@ import lombok.ToString;
 @Getter
 @Setter
 public class LogiExportVo {
-	String status;
-	String location;
 	String instruction_no;
-	String customer;
+	String status;
 	String lot_no;
-	String item_no;
+	String item_code;
 	String item_name;
+	int order_amount;
+	int ex_amount;
+	int ex_remain;
+	String unit;
 	float width;
 	float weight;
 	float thickness;
 	float height;
-	int order_amount;
-	int ex_amount;
-	int ex_remain;
+	String product_family;
+	String location;
+	String from_warehouse;
+	String customer;
 	String order_date;
 	String inst_reg_date;
 	String inst_deadline;
